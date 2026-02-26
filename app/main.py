bba8686d8ff5414c18a8f2607eac737f87e072ef
blanca <blanca.pueche@cnb.csic.es>
2026-02-26 15:00:19 +0100

    budget and year

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # Set root folder for the project & app path
 project_root = Path(__file__).resolve().parent.parent
-app_path = project_root / "app" / "home_page2.py"
+app_path = project_root / "app" / "home_page.py"
 
 # Launch Streamlit
 subprocess.run([sys.executable, "-m", "streamlit", "run", str(app_path)])
--- app/test.py
+++
@@ -1,4 +0,0 @@
-from utils.pipeline import *
-
-aids = ['A5050710342', 'A5071564228', 'A5039659064']
-print(get_inst_ids_from_authors(aids))
